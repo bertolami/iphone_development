a2322eeb3acea67f628ef086fa506207afcc827b
Bertolami <roman@bertolami.net>
2010-01-27 20:37:49 +0100

    movie

---
+++ Movie/Classes/Movie.m
@@ -0,0 +1,37 @@
+//
+//  Movie.m
+//  Movie
+//
+//  Created by Roman Bertolami on 27.01.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "Movie.h"
+
+
+@implementation Movie
+
+@synthesize title;
+@synthesize boxOfficeGross;
+@synthesize summary;
+
+- (id) initWithTitle: (NSString*) newTitle 
+			boxOfficeGross: (NSNumber*) newBoxOfficeGross
+						 summary: (NSString*) newSummary {
+	self = [super init];
+	if(nil != self) {
+		self.title = newTitle;
+		self.boxOfficeGross = newBoxOfficeGross;
+		self.summary = newSummary;
+	}
+	return self;
+}
+
+- (void) dealloc {
+	self.title = nil;
+	self.boxOfficeGross = nil;
+	self.summary = nil;
+	[super dealloc];
+}
+
+@end
